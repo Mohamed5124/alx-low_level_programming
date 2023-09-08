65510ba87320e6199883b4cf5e8dbd08b5f5855d
Mohamed5124 <Realdarkf@gmail.com>
2023-09-08 07:28:44 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * MAIN - Entery point.
+ *
+ * Description: print all single digit numbers.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+	}
+	printf("\n");
+
+	return (0);
+}
